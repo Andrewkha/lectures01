96a501eb44b9ca1268bfaf1269aeed2c4fd19ee0
Andrewkha <chernyshov.andrey@gmail.com>
2020-05-11 23:36:15 +0300

    practice 02 - tasks 01 - 08 completed

--- robot-tasks-master/task_7.py
+++ robot-tasks-master/task_7.py
@@ -5,7 +5,17 @@ from pyrob.api import *
 
 @task
 def task_5_4():
-    pass
+    while not wall_is_beneath():
+        move_down()
+
+    length = 0
+    while wall_is_beneath():
+        move_right()
+        length += 1
+
+    move_down()
+
+    move_left(length)
 
 
 if __name__ == '__main__':
