72739e308d60b0487ca7c4ba99626cb71346c23a
Andrewkha <chernyshov.andrey@gmail.com>
2020-05-11 10:24:21 +0300

    practice 01 - mult squares, spider

--- practice01.py
+++ practice01.py
@@ -16,15 +16,12 @@ from math import pi
 
 # square
 
-# turtle.shape('turtle')
-# turtle.forward(50)
-# turtle.left(90)
-# turtle.forward(50)
-# turtle.left(90)
-# turtle.forward(50)
-# turtle.left(90)
-# turtle.forward(50)
-# turtle.left(90)
+
+def square(side):
+    turtle.shape('turtle')
+    for step in range(0, 4):
+        turtle.forward(side)
+        turtle.left(90)
 
 # circle
 
@@ -36,3 +33,26 @@ def circle(radius, n=100):
         turtle.left(360 / n)
 
 
+def mult_squares(step, side):
+    x, y = turtle.position()
+    for _ in range(0, step):
+        turtle.penup()
+        turtle.goto(x, y)
+        turtle.pendown()
+        square(side)
+        x, y = x - 5, y - 5
+        side += 10
+
+
+def spider(n):
+    turtle.shape("turtle")
+    for _ in range(0, n):
+        turtle.penup()
+        turtle.goto(0, 0)
+        turtle.pendown()
+        turtle.left(360 / n)
+        turtle.forward(100)
+        turtle.stamp()
+
+
+spider(10)
