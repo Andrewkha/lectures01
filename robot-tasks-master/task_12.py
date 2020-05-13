a4b48d66e14b1e1816091bce8ae19ff3bc045177
Andrey Chernyshov <andrey.chernyshov@intel.com>
2020-05-13 11:49:13 +0300

    Taks 10-14 done

--- robot-tasks-master/task_12.py
+++ robot-tasks-master/task_12.py
@@ -5,8 +5,14 @@ from pyrob.api import *
 
 @task
 def task_8_6():
-    pass
 
+    while True:
+        if wall_is_beneath() and not wall_is_above():
+            fill_cell()
+        if not wall_is_on_the_right():
+            move_right()
+        else:
+            break
 
 if __name__ == '__main__':
     run_tasks()
